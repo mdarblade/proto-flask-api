1f07a92ae6b5740e693f1ea681096e07a8cb6091
Matthias Darblade <matthias.darblade@gmail.com>
2022-04-13 17:31:31 -0300

    fix: fix makedile

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import pyarrow.parquet as pq
-import s3fs
-
-import pandas as pd
-from flask import Flask, request
-from protopython.proto import query_pb2
 import io
 
+import geopandas as gpd
+import pandas as pd
+import pyarrow.parquet as pq
+import s3fs
+from flask import Flask, request
+
+from protopython.proto import query_pb2
 
 APP = Flask(__name__)
 CACHED_DATA: pd.DataFrame
@@ -29,14 +30,46 @@ def load_dataframe(path: str) -> pd.DataFrame:
     return CACHED_DATA.copy()
 
 
+def shape_filter(dataframe: pd.DataFrame, shapes: list) -> pd.DataFrame:
+    """
+    Filter a pandas dataframe using a list of shapes
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to filter
+        shapes (list): List of shapes
+
+    Returns:
+        pd.DataFrame: 
+    """
+    gdf = gpd.GeoDataFrame(
+        dataframe,
+        geometry=gpd.points_from_xy(dataframe.situslongitude, dataframe.situslatitude),
+    )
+
+    for i, shape in enumerate(shapes):
+        if i == 0:
+            shape_filter = gdf.within(shape)
+        else:
+            shape_filter &= gdf.within(shape)
+
+    return pd.DataFrame(gdf[shape_filter])
+
+
 @APP.route("/count-properties", methods=["GET", "POST"])
 def get_request():
     # We use the proto object to read and write the responses
     query_parameters = query_pb2.QueryProperties()
-
     query_parameters.ParseFromString(request.data)
 
     print(query_parameters)
+
+    df = load_dataframe()
+
+    df = shape_filter(df, [query_parameters.shape])
+
+    # Not implemented
+    # df = apply_preset_filters(df, query_parameters.preset_filter)
+    # df = apply_filters(df, query_parameters.filters)
 
     query_response = query_pb2.QueryCountResponse()
     query_response.count = 42
--- lib/filters.py
+++ lib/filters.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from dataclasses import dataclasses
 
-
-FILTERS = {
+# https://docs.google.com/spreadsheets/d/1wsnMLD1jB_Y5LGb32qKPneozJUwvMLMkqq6ZqiMBN0g/edit?usp=sharing
+PRESET_FILTERS = {
     0: lambda df: df.year_build > 2005,
     1: lambda df: df.land_use_code == "1001",
 }
